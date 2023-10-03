71d2f1ba32395b1cdc19322e8890b16b9c593abf
tiago.encarnacao <tiago.encarnacao@valtech.com>
2023-10-03 17:46:47 +0200

    feat: setup cypress

---
+++ cypress/cypress.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig } from "cypress";
+import * as createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  e2e: {
+    specPattern: ["**/*.feature"],
+    supportFile: "support/e2e.ts",
+    videosFolder: "report/videos",
+    screenshotsFolder: "report/screenshots",
+    reporter: require.resolve(
+        "@badeball/cypress-cucumber-preprocessor/pretty-reporter",
+    ),
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+          "file:preprocessor",
+          createBundler({
+            plugins: [createEsbuildPlugin(config)],
+          }),
+      );
+      on("task", {
+        log(message) {
+          // eslint-disable-next-line no-console
+          console.log(message);
+          return null;
+        },
+      });
+      return config;
+    },
+  },
+});
